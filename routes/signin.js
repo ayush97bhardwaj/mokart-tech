5803e696d7d50d2803e6c1566d605d2f0ee3b480
ayush97bhardwaj <ayush97bhardwaj@gmail.com>
2018-04-08 17:06:14 +0530

    seller backend

--- routes/signin.js
+++ routes/signin.js
@@ -11,12 +11,12 @@ router.post('/customer',(req,res,next)=>{
     db.query(pass,(err,result)=>{
         if(err) console.log(err);
         else if(result.length==0){
-            res.render('signin',{err:'email not registered'});
+            res.render('signin',{err:'email not registered as customer'});
         }
         else{
             // console.log(result[0].password);
             if(result[0].password != req.body.password){
-                res.render('signin',{err:'wrong password',email:req.body.email});
+                res.render('signin',{err:'wrong password for registered customer',email:req.body.email});
             }
             else res.redirect('/signin');
         }
@@ -24,4 +24,21 @@ router.post('/customer',(req,res,next)=>{
     // res.redirect('/signin');
 });
 
+router.post('/seller',(req,res,next)=>{
+    var pass="SELECT * FROM sellers as s WHERE s.sellerid='"+req.body.email+"'";
+    db.query(pass,(err,result)=>{
+        if(err) console.log(err);
+        else if(result.length==0){
+            res.render('signin',{err:'email not registered for seller'});
+        }
+        else{
+            // console.log(result[0].password);
+            if(result[0].password != req.body.password){
+                res.render('signin',{err:'wrong password for registered seller',email:req.body.email});
+            }
+            else res.redirect('/signin');
+        }
+    })
+    // res.redirect('/signin');
+});
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -94,4 +94,35 @@ router.post('/customer', function(req, res, next) {
 router.get('/seller', function(req, res, next) {
     res.render('signup_seller', { title: 'Seller SignUp' });
 });
+router.post('/seller',function(req,res,next){
+    var create_table_sellers="CREATE TABLE IF NOT EXISTS sellers (sellerid VARCHAR(40) NOT NULL UNIQUE,sname VARCHAR(20) NOT NULL,password VARCHAR(100) NOT NULL,dob DATE NOT NULL,address VARCHAR(100) NOT NULL,PRIMARY KEY (custid))";
+    db.query(create_table_sellers,function(err,result){
+        if(err) console.log(err);
+        else console.log(result);
+    });
+
+    var create_table_sellerphone="CREATE TABLE IF NOT EXISTS sellerphone (sellerid VARCHAR(40) NOT NULL,sphone VARCHAR(20) NOT NULL,PRIMARY KEY (sellerid,sphone),FOREIGN KEY (sellerid) REFERENCES sellers(sellerid))";
+    db.query(create_table_sellerphone,function(err,result){
+        if(err) console.log(err);
+        else console.log(result);
+    });
+
+    //...................insert data of seller...............................
+
+    var insert_seller="INSERT INTO sellers (sellerid,sname,password,dob,address) values ('" + req.body.email+ "','"+req.body.sname+"','"+req.body.password+"','"+req.body.dob+"','"+req.body.address+"')";
+    db.query(insert_seller,function(err,result){
+        if(err) console.log(err);
+        else console.log(result);
+    });
+
+    req.body.phone.forEach(phone => {
+        var insert_sellerphone="INSERT INTO sellerphone (sellerid,sphone) values ('" + req.body.email+ "','"+phone+"')";
+        db.query(insert_sellerphone,function(err,result){
+            if(err) console.log(err);
+            else console.log(result);
+        });
+    });
+
+    res.redirect('/');
+});
 module.exports = router;
