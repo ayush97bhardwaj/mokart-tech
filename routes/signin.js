686b33e0638e55e7784191a999d5076b53bb6613
ayush97bhardwaj <ayush97bhardwaj@gmail.com>
2018-04-16 23:40:57 +0530

    session ready

--- routes/signin.js
+++ routes/signin.js
@@ -6,18 +6,28 @@ router.get('/', function(req, res, next) {
     console.log(1);
     if(req.session){
         console.log(12);
-        if(req.session.custid){
+        if(req.session.user){
             console.log(123);
-            res.render('signin', { title: 'signin' , user:req.session.custid});
+            // var cust='select custid,cname from customers where custid='+req.session.custid;
+            // db.query(cust,(err,result)=>{
+            //     console.log(result);
+            //     res.render('signin', { title: 'signin' , user:result});
+            // });
+            res.render('/');
         }
-        else if(req.session.sellerid){
-            console.log(1234);
-            res.render('signin', { title: 'signin' , user:req.session.sellerid});
-        }
+        // else if(req.session.sellerid){
+        //     console.log(1234);
+        //     // var cust='select sellerid,sname from sellers where sellerid='+req.session.sellerid;
+        //     // db.query(cust,(err,result)=>{
+        //     //     console.log(result);
+        //     //     res.render('signin', { title: 'signin' , user:result});
+        //     // });
+        //     res.render('signin', { title: 'signin' , user:result});
+        // }
         else{
             console.log(12345);
             console.log(req.session);
-            res.render('signin', { title: 'signin' });
+            res.render('signin', { title: 'signin',loggedin:false});
         }
     }
     else{
@@ -39,28 +49,37 @@ router.post('/customer',(req,res,next)=>{
                 res.render('signin',{err:'wrong password for registered customer',email:req.body.email});
             }
             else {
-                req.session.custid=req.body.email;
-                var shoppingcart="CREATE TABLE IF NOT EXISTS shoppingcart (cartid VARCHAR(40) NOT NULL UNIQUE,custid VARCHAR(40) NOT NULL,orderid VARCHAR(40),paymode VARCHAR(20),orderdate DATE,orderstatus VARCHAR(20),PRIMARY KEY (cartid),FOREIGN KEY (custid) REFERENCES customers(custid))"; 
-                db.query(shoppingcart,(err,result)=>{
+                var countquery="select * from customers where custid='"+req.body.email+"'";
+                db.query(countquery,(err,result)=>{
                     if(err) console.log(err);
-                    // else console.log(result);
+                    else{
+                        req.session.user=result[0];
+                        req.session.user.type='customer';
+                        delete req.session.user.password;
+                        // req.session.custid=req.body.email;
+                        var shoppingcart="CREATE TABLE IF NOT EXISTS shoppingcart (cartid VARCHAR(40) NOT NULL UNIQUE,custid VARCHAR(40) NOT NULL,orderid VARCHAR(40),paymode VARCHAR(20),orderdate DATE,orderstatus VARCHAR(20),PRIMARY KEY (cartid),FOREIGN KEY (custid) REFERENCES customers(custid))"; 
+                        db.query(shoppingcart,(err,result)=>{
+                            if(err) console.log(err);
+                            // else console.log(result);
+                        });
+                        var addedto="CREATE TABLE IF NOT EXISTS addedto (itemid VARCHAR(40) NOT NULL,addedqty INT NOT NULL,cartid VARCHAR(40) NOT NULL,PRIMARY KEY(itemid,cartid),FOREIGN KEY(itemid) REFERENCES items(itemid),FOREIGN KEY(cartid) REFERENCES shoppingcart(cartid))";
+                        db.query(addedto,(err,result)=>{
+                            if(err) console.log(err);
+                            // else console.log(result);
+                        });
+                        var contains="CREATE TABLE IF NOT EXISTS contains (orderid VARCHAR(40) NOT NULL,itemid VARCHAR(40) NOT NULL,orderedqty INT NOT NULL,dateofship DATE NOT NULL,dateofdelivery DATE NOT NULL,PRIMARY KEY(orderid,itemid),FOREIGN KEY(itemid) REFERENCES items(itemid))";
+                        db.query(contains,(err,result)=>{
+                            if(err) console.log(err);
+                            // else console.log(result);
+                        });
+                        var views="CREATE TABLE IF NOT EXISTS views (custid VARCHAR(40) NOT NULL,itemid VARCHAR(40) NOT NULL, PRIMARY KEY(custid,itemid),FOREIGN KEY(itemid) REFERENCES items(itemid),FOREIGN KEY(custid) REFERENCES customers(custid))";
+                        db.query(views,(err,result)=>{
+                            if(err) console.log(err);
+                            // else console.log(result);
+                        });
+                        res.redirect('/signin');
+                    }
                 });
-                var addedto="CREATE TABLE IF NOT EXISTS addedto (itemid VARCHAR(40) NOT NULL,addedqty INT NOT NULL,cartid VARCHAR(40) NOT NULL,PRIMARY KEY(itemid,cartid),FOREIGN KEY(itemid) REFERENCES items(itemid),FOREIGN KEY(cartid) REFERENCES shoppingcart(cartid))";
-                db.query(addedto,(err,result)=>{
-                    if(err) console.log(err);
-                    // else console.log(result);
-                });
-                var contains="CREATE TABLE IF NOT EXISTS contains (orderid VARCHAR(40) NOT NULL,itemid VARCHAR(40) NOT NULL,orderedqty INT NOT NULL,dateofship DATE NOT NULL,dateofdelivery DATE NOT NULL,PRIMARY KEY(orderid,itemid),FOREIGN KEY(itemid) REFERENCES items(itemid))";
-                db.query(contains,(err,result)=>{
-                    if(err) console.log(err);
-                    // else console.log(result);
-                });
-                var views="CREATE TABLE IF NOT EXISTS views (custid VARCHAR(40) NOT NULL,itemid VARCHAR(40) NOT NULL, PRIMARY KEY(custid,itemid),FOREIGN KEY(itemid) REFERENCES items(itemid),FOREIGN KEY(custid) REFERENCES customers(custid))";
-                db.query(views,(err,result)=>{
-                    if(err) console.log(err);
-                    // else console.log(result);
-                });
-                res.redirect('/signin');
             }
         }
     })
@@ -80,8 +99,17 @@ router.post('/seller',(req,res,next)=>{
                 res.render('signin',{err:'wrong password for registered seller',email:req.body.email});
             }
             else {
-                req.session.sellerid=req.body.email;
-                res.redirect('/signin');
+                // req.session.sellerid=req.body.email;
+                var countquery="select * from sellers where sellerid='"+req.body.email+"'";
+                db.query(countquery,(err,result)=>{
+                    if(err) console.log(err);
+                    else{
+                        req.session.user=result[0];
+                        req.session.user.type='seller';
+                        delete req.session.user.password;
+                        res.redirect('/signin');
+                    }
+                });
             }
         }
     })
