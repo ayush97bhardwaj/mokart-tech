db2ce89fb20208838484ec851389271e7255cc54
ayush97bhardwaj <ayush97bhardwaj@gmail.com>
2018-04-21 00:25:51 +0530

    sign up constraints set

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,33 @@ router.get('/', function(req, res, next) {
   //   }
   // });
 
-  var countquery="select * from items";
-  db.query(countquery,(err,result)=>{
+  var itemsexists=`SELECT count(*)
+      FROM information_schema.tables
+      WHERE table_schema = 'dbms_proj' 
+      AND table_name = 'items'`;
+  
+  db.query(itemsexists,(err,result)=>{
     if(err) console.log(err);
     else{
-      if(req.session.user)
-        res.render('index', { title: 'Home',items:result,loggedin:true,user:req.session.user});
-      else
-        res.render('index', { title: 'Home',items:result,loggedin:false});
+      // console.log(result[0]);
+      if(result[0]['count(*)']!=0){
+        console.log('items are present');
+        var countquery="select * from items";
+        db.query(countquery,(err,result)=>{
+          if(err) console.log(err);
+          else{
+            if(req.session.user)
+              res.render('index', { title: 'Home',items:result,loggedin:true,user:req.session.user});
+            else
+              res.render('index', { title: 'Home',items:result,loggedin:false});
+          }
+        });
+      }
+      else{
+        res.render('index', { title: 'Home',items:0,loggedin:false});
+      }
     }
-  });
+  });    
+
 });
 module.exports = router;
